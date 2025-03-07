68554a82eb985c75dbbdcfedc1f36b3a07d84b4b
MoonBow-1 <49@teragrep.com>
2025-03-07 15:58:22 +0200

    Migrate javax.annotation.PostConstruct to jakarta.annotation.PostConstruct

--- src/main/java/com/teragrep/cfe_16/SessionManager.java
+++ src/main/java/com/teragrep/cfe_16/SessionManager.java
@@ -53,7 +53,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
