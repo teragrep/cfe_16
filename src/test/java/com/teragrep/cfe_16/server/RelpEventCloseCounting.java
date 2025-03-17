b4e76ca23dc4ec50d3448917a5c5a0e604a5e64b
MoonBow-1 <49@teragrep.com>
2025-03-17 16:38:06 +0200

    Change headers to say when copyright begins

--- src/test/java/com/teragrep/cfe_16/server/RelpEventCloseCounting.java
+++ src/test/java/com/teragrep/cfe_16/server/RelpEventCloseCounting.java
@@ -1,6 +1,6 @@
 /*
  * HTTP Event Capture to RFC5424 CFE_16
- * Copyright (C) 2025 Suomen Kanuuna Oy
+ * Copyright (C) 2019-2025 Suomen Kanuuna Oy
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
