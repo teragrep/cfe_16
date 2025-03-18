005d98478da270d1486b674a46e3902f3d1740ee
MoonBow-1 <49@teragrep.com>
2025-03-18 09:32:43 +0200

    Fix tests in SendEventsIT

--- src/test/java/com/teragrep/cfe_16/it/SendEventsIT.java
+++ src/test/java/com/teragrep/cfe_16/it/SendEventsIT.java
@@ -46,12 +46,6 @@
 package com.teragrep.cfe_16.it;
 
 import com.teragrep.cfe_16.service.HECService;
-import org.junit.jupiter.api.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.test.context.TestPropertySource;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -62,35 +56,48 @@ import java.nio.channels.Selector;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.TestPropertySource;
 
 @TestPropertySource(properties = {
-        "syslog.server.host=127.0.0.1",
-        "syslog.server.port=1236",
-        "syslog.server.protocol=TCP",
-        "max.channels=1000000",
-        "max.ack.value=1000000",
-        "max.ack.age=20000",
-        "max.session.age=30000",
-        "poll.time=30000",
-        "server.print.times=true"
+    "syslog.server.host=127.0.0.1",
+    "syslog.server.port=1236",
+    "syslog.server.protocol=TCP",
+    "max.channels=1000000",
+    "max.ack.value=1000000",
+    "max.ack.age=20000",
+    "max.session.age=30000",
+    "poll.time=30000",
+    "server.print.times=true"
 })
 @SpringBootTest
 public class SendEventsIT implements Runnable {
 
+    private final static int SERVER_PORT = 1236;
     @Autowired
     private HECService service;
-
     private ServerSocket serverSocket;
     private Thread thread;
     private AtomicInteger numberOfRequestsMade;
-
     private MockHttpServletRequest request1;
     private String eventInJson;
     private String channel1;
     private Selector selector;
-    private final static int SERVER_PORT = 1236;
     private CountDownLatch countDownLatch = new CountDownLatch(0);
 
     @BeforeEach
@@ -103,7 +110,10 @@ public class SendEventsIT implements Runnable {
         this.request1 = new MockHttpServletRequest();
         this.request1.addHeader("Authorization", "AUTH_TOKEN_11111");
         this.channel1 = "CHANNEL_11111";
-        this.eventInJson = "{\"sourcetype\":\"access\", \"source\":\"/var/log/access.log\", \"event\": {\"message\":\"Access log test message 1\"}} {\"sourcetype\":\"access\", \"source\":\"/var/log/access.log\", \"event\": {\"message\":\"Access log test message 2\"}}";
+        this.eventInJson = "{\"sourcetype\":\"access\", \"source\":\"/var/log/access.log\", "
+            + "\"event\": {\"message\":\"Access log test message 1\"}} "
+            + "{\"sourcetype\":\"access\", \"source\":\"/var/log/access.log\", \"event\": "
+            + "{\"message\":\"Access log test message 2\"}}";
 
         this.thread.start();
     }
@@ -117,7 +127,8 @@ public class SendEventsIT implements Runnable {
         while (true) {
             try {
                 Socket socket = this.serverSocket.accept();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
                 String line;
                 while ((line = bufferedReader.readLine()) != null) {
                     this.numberOfRequestsMade.getAndIncrement();
@@ -140,15 +151,15 @@ public class SendEventsIT implements Runnable {
     }
 
     @Test
-    public void sendEventsTest() throws IOException, InterruptedException, ExecutionException {
+    public void sendEventsTest() throws InterruptedException, ExecutionException {
         int NUMBER_OF_EVENTS_TO_BE_SENT = 100;
         countDownLatch = new CountDownLatch(NUMBER_OF_EVENTS_TO_BE_SENT);
         ExecutorService es = Executors.newFixedThreadPool(8);
         List<CompletableFuture<String>> futures = new ArrayList<>();
 
         for (int i = 0; i < NUMBER_OF_EVENTS_TO_BE_SENT; i++) {
-            CompletableFuture<String> f = CompletableFuture
-                    .supplyAsync(() -> service.sendEvents(request1, channel1, eventInJson).toString());
+            CompletableFuture<String> f = CompletableFuture.supplyAsync(
+                () -> service.sendEvents(request1, channel1, eventInJson).toString());
             futures.add(f);
         }
         List<String> supposedResponses = new ArrayList<String>();
@@ -159,8 +170,9 @@ public class SendEventsIT implements Runnable {
         int countFuture = 0;
         for (Future<String> f : futures) {
             final String actualResponse = f.get();
-            Assertions
-                    .assertTrue(supposedResponses.contains(actualResponse), "Service should return JSON object with fields 'text', 'code' and 'ackID' (ackID should be " + countFuture + ")");
+            Assertions.assertTrue(supposedResponses.contains(actualResponse),
+                "Service should return JSON object with fields 'text', 'code' and 'ackID' (ackID "
+                    + "should be " + countFuture + ")");
             countFuture++;
         }
         countDownLatch.await(1, TimeUnit.SECONDS);
@@ -174,16 +186,17 @@ public class SendEventsIT implements Runnable {
 
     @Disabled
     @Test
-    public void send1EventTest() throws IOException, InterruptedException {
+    public void send1EventTest() throws InterruptedException {
         countDownLatch = new CountDownLatch(1);
         String supposedResponse = "{\"text\":\"Success\",\"code\":0,\"ackID\":" + 0 + "}";
-        Assertions
-                .assertEquals(service.sendEvents(request1, channel1, eventInJson).toString(), supposedResponse, "Service should return JSON object with fields 'text', 'code' and 'ackID' (ackID should be " + 0 + ")");
+        Assertions.assertEquals(supposedResponse,
+            service.sendEvents(request1, channel1, eventInJson).toString(),
+            "Service should return JSON object with fields 'text', 'code' and 'ackID' (ackID "
+                + "should be "
+                + 0 + ")");
 
         countDownLatch.await(5, TimeUnit.SECONDS);
-        Assertions
-                .assertEquals(
-                        2, this.numberOfRequestsMade, "Number of events received should match the number of sent ones"
-                );
+        Assertions.assertEquals(2, this.numberOfRequestsMade.get(),
+            "Number of events received should match the number of sent ones");
     }
 }
