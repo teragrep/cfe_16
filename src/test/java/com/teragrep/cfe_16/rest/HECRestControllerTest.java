adebd4024c995f7faff7b94734dcfea36bac9410
MoonBow-1 <146731818+MoonBow-1@users.noreply.github.com>
2026-04-15 10:59:52 +0300

    Fix variable names in HECRestControllerTest
    - "variable" renamed to "responseEntity"
    - "jsonNodeResponseEntity" renamed to "expectedResponseEntity"

--- src/test/java/com/teragrep/cfe_16/rest/HECRestControllerTest.java
+++ src/test/java/com/teragrep/cfe_16/rest/HECRestControllerTest.java
@@ -141,12 +141,12 @@ class HECRestControllerTest {
                 + "{\"sourcetype\":\"access\", \"source\":\"/var/log/access.log\", \"event\": "
                 + "{\"message\":\"Access log test message 2\"}}";
 
-        final ResponseEntity<JsonNode> variable = Assertions
+        final ResponseEntity<JsonNode> responseEntity = Assertions
                 .assertDoesNotThrow(() -> this.hecRestController.sendEvents(request1, eventInJson, null));
         final JsonResponse expectedResponse = new JsonResponse("Success");
-        final ResponseEntity<JsonNode> jsonNodeResponseEntity = expectedResponse.asJsonNodeResponseEntity();
+        final ResponseEntity<JsonNode> expectedResponseEntity = expectedResponse.asJsonNodeResponseEntity();
 
-        Assertions.assertEquals(jsonNodeResponseEntity, variable);
+        Assertions.assertEquals(expectedResponseEntity, responseEntity);
     }
 
     @Test
